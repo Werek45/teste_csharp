0a7af494189e6caddedd3ba37814ac4279b2f8fa
Werek Silva <45werek@gmail.com>
2022-06-03 17:03:32 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!\n");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeiroProjeto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                int cont = 1 ;
+
+
+            for (cont = 1; cont <=3; cont++)
+            {
+                    double nota1=0;
+               
+                    Console.Write("\nPrimeira nota: ");
+                    nota1 = double.Parse(Console.ReadLine());
+
+                double nota2;
+                
+                    Console.Write("Segunda nota: ");
+                    nota2 = double.Parse(Console.ReadLine());
+                
+                double m ;
+                m = (nota1 + nota2) / 2;
+
+                Console.WriteLine($"\tA media do {cont}° aluno foi " + m);
+                if (m >= 7)
+                {
+                    Console.WriteLine($"{cont}° aluno, APROVADRO!");
+                    if ((m >= 5) && (m < 7))
+                    {
+                        Console.WriteLine($"{cont}° aluno, em RECUPERÇAO! ");
+                    }
+                }
+                else 
+                {
+                    Console.WriteLine($"{cont}° aluno, REPROVADO!"); 
+                }
+
+            }
+               
+            
+
+            Console.WriteLine("\n\n\tPrecione qualquer tecla para sair!!");
+            Console.ReadKey();
+        }
+    }
+}
